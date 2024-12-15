bc072ec2dc5eb16676bdf94869c41800815b16a3
Youssef-Bahaa <162743130+Youssef-Bahaa@users.noreply.github.com>
2024-12-15 14:10:12 +0200

    More generic code
    Used function pointer instead of duplicating code. 

--- main.cpp
+++ main.cpp
@@ -3,77 +3,17 @@
 #include "Run.h"
 using namespace std;
 
-
-void displayMainMenu() {
-    cout << "\n===== Main Menu =====\n";
-    cout << "1) Numerical Xo Game\n";
-    cout << "2) Connect_4 Game\n";
-    cout << "3) Ultimate Xo\n";
-    cout << "4)Exit.\n";
-    cout << "=====================\n";
-    cout << "Enter your choice: ";
-}
-
-void numericalXoLoop() {
-    RunNumXO();
+void gameLoop(void (*runGame)()) {
+    runGame();
     while (true) {
-
         string c;
-        cout << "\n--- Numerical Xo Game ---\n";
-        cout << "1) Continue the Game\n";
+        cout << "\n1) Continue the Game\n";
         cout << "2) Back to Main Menu\n";
         cout << "Enter your choice: ";
         cin >> c;
 
         if (c == "1") {
-            RunNumXO();
-            continue; 
-        }
-        else if (c == "2") {
-            break;
-        }
-        else {
-            cout << "Invalid choice. Please select 1 or 2.\n";
-        }
-    }
-}
-
-void connect4Loop() {
-    RunConnect4();
-    while (true) {
-        string c;
-        cout << "\n--- Connect_4 Game ---\n";
-        cout << "1) Continue the Game\n";
-        cout << "2) Back to Main Menu\n";
-        cout << "Enter your choice: ";
-        cin >> c;
-
-        if (c == "1") {
-            RunConnect4();
-            continue; 
-        }
-        else if (c == "2") {
-            break; 
-        }
-        else {
-            cout << "Invalid choice. Please select 1 or 2.\n";
-        }
-    }
-}
-
-
-void UltimateLoop() {
-    RunUltimate();
-    while (true) {
-        string c;
-        cout << "\n--- Ultimate Xo Game ---\n";
-        cout << "1) Continue the Game\n";
-        cout << "2) Back to Main Menu\n";
-        cout << "Enter your choice: ";
-        cin >> c;
-
-        if (c == "1") {
-            RunUltimate();
+            runGame();
             continue;
         }
         else if (c == "2") {
@@ -85,9 +25,18 @@ void UltimateLoop() {
     }
 }
 
-
-
-
+void displayMainMenu() {
+    cout << "\n===== Main Menu =====\n";
+    cout << "1) Numerical Xo Game\n";
+    cout << "2) Connect_4 Game\n";
+    cout << "3) Ultimate Xo\n";
+    cout << "4) Game 3\n";
+    cout << "5) Game 6\n";
+    cout << "6) Game 7\n";
+    cout << "7) Exit\n";
+    cout << "=====================\n";
+    cout << "Enter your choice: ";
+}
 
 int main() {
     while (true) {
@@ -96,27 +45,31 @@ int main() {
         cin >> choice;
 
         if (choice == "1") {
-            numericalXoLoop(); 
+            gameLoop(RunNumXO);
         }
         else if (choice == "2") {
-            connect4Loop(); 
+            gameLoop(RunConnect4);
         }
         else if (choice == "3") {
-            UltimateLoop();
+            gameLoop(RunUltimate);
         }
         else if (choice == "4") {
+            gameLoop(RunGame3);
+        }
+        else if (choice == "5") {
+            gameLoop(RunGame6);
+        }
+        else if (choice == "6") {
+            gameLoop(RunGame7);
+        }
+        else if (choice == "7") {
             cout << "Exiting the program. Goodbye!\n";
-            break; 
+            break;
         }
         else {
-            cout << "Invalid choice. Please select a valid option (1, 2, or 3).\n";
+            cout << "Invalid choice. Please select a valid option (1-7).\n";
         }
     }
 
     return 0;
 }
-
-
-
-
-
